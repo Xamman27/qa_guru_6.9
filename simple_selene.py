59aedcbd653262ba5a9ba927bc8e4aa553f42887
Xamman27 <xamman27@gmail.com>
2023-08-11 23:41:49 +0400

    Added simple_selene.py

--- simple_selene.py
+++ simple_selene.py
@@ -1,12 +1,15 @@
 from selene.support.shared import browser
 from selene.support import by
 from selene.support.conditions import be
+import pytest
 
-browser.open("https://github.com")
-browser.element(".header-search-button").click()
-browser.element("#query-builder-test").send_keys("eroshenkoam/allure-example")
-browser.element("#query-builder-test").press_enter()
 
-browser.element('[class="Item__LiBox-sc-yeql7o-0 kUyONJ"]').click()
+def test_github():
+    browser.open("https://github.com")
+    browser.element(".header-search-button").click()
+    browser.element("#query-builder-test").send_keys("eroshenkoam/allure-example")
+    browser.element("#query-builder-test").press_enter()
 
-browser(by.partial_text("#8")).should(be.visible)
+    browser.element('[id=":r8:"]').click()
+
+    browser.element(by.partial_text("#1")).should(be.visible)#Тест падает не пойму причину
